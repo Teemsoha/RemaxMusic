1bc578b0cf83d4d0e107216e7b9dcc29b1cc8064
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-03-23 07:10:51 +0300

    Create minfekol.py

---
+++ RemaxBot/plugins/Remax2/minfekol.py
@@ -0,0 +1,32 @@
+from pyrogram import filters, Client
+from RemaxBot import app
+import asyncio
+from pytgcalls import PyTgCalls, StreamType
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from RemaxBot.core.call import Remax 
+from RemaxBot.utils.database import *
+from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError)
+
+@app.on_message(filters.regex("مين في الكول"))
+async def strcall(client, message):
+    assistant = await group_assistant(Remax,message.chat.id)
+    try:
+        await assistant.join_group_call(message.chat.id, AudioPiped("./strings/call.mp3"), stream_type=StreamType().pulse_stream)
+        text="↯︙الاعضاء المتواجدين في المكالمة المرئية :\n\n"
+        participants = await assistant.get_participants(message.chat.id)
+        k =0
+        for participant in participants:
+            info = participant
+            if info.muted == False:
+                mut="⦗ يتكلم ⦘"
+            else:
+                mut="⦗ لا يتكلم ⦘"
+            user = await client.get_users(participant.user_id)
+            k +=1
+            text +=f"{k}:{user.mention}↬{mut}\n"
+        text += f"\n↯︙عدد الاشخاص في المكالمة المرئية ↬ ⦗ {len(participants)} ⦘"    
+        await message.reply(f"{text}")
+        await asyncio.sleep(5)
+        await assistant.leave_group_call(message.chat.id)
+    except NoActiveGroupCall:
+        await message.reply(f"لم يتم العثور على دردشة فيديو نشطة.\nيرجى بدء دردشة فيديو في مجموعتك/قناتك والمحاولة مرة أخرى.")
